dc38da32d7edcec4d1be8803510da85a164832bf
chenyangkang <chenyangkang24@outlook.com>
2025-08-28 13:44:34 +0000

    add model collection code

---
+++ scripts/11.Collect_the_best_models/collect_best_models.R
@@ -0,0 +1,18 @@
+library(ggplot2)
+setwd('/home/yc85_illinois_edu/BirdFlow_Validation_Project/scripts/11.Collect_the_best_models/')
+source('../plotting_params/plotting_params.R')
+results <- read.csv('../../data/03.All_validation_summary/validation_final_summary.csv')
+results <- results |> dplyr::group_by(.data[['sp']], .data[['method']]) |> dplyr::slice(1) |> dplyr::ungroup()
+results <- results[results$method=='ST098_and_LL',]
+
+for (row_idx in 1:nrow(results)) {
+  print(row_idx)
+  sp_code <- results[row_idx, ]$sp
+  source_file <- glue::glue('/project/pi_drsheldon_umass_edu/birdflow/batch_model_validation/model_output_hyperparams_distance_metric/{sp_code}/{sp_code}_150km/{results[row_idx, ]$model}')
+  target_folder <- glue::glue('/home/yc85_illinois_edu/BirdFlow_Validation_Project/models/{sp_code}')
+  if (!dir.exists(target_folder)) {
+    dir.create(target_folder, recursive = TRUE)
+  }
+  
+  file.copy(source_file, glue::glue("{target_folder}/{sp_code}_tuned.hdf5"))
+}
